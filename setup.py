b3803c245ad7b072d229963feb223c83c845900d
kdowolski <krystian.dowolski@reef.pl>
2022-06-14 01:00:40 +0200

    tmp

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testapp",
+    version="0.0.1",
+    url="https://github.com/kdowolski-reef/test_repo",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
